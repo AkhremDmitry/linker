39979f1c30e88a575f26cc931d228c1ff2e06b56
reisin1986@gmail.com <reisin1986@gmail.com>
2018-06-25 23:56:59 +0300

    fix methods

--- src/main/java/com/asuscomm/reisin/linker/model/Group.java
+++ src/main/java/com/asuscomm/reisin/linker/model/Group.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "Group")
+@Entity(name = "links_group")
 public class Group {
 
     @Id
